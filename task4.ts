f0e0b6832c6e7d969d82ec3226931e30fa025d0b
mjaqamy <mjaqamy@gmail.com>
2026-02-08 18:36:12 +0300

    feat: resolve task4L7

--- task4.ts
+++ task4.ts
@@ -1,6 +1,18 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 // Написать функцию, которая возвращает объект в виде ключ (элемент массива) и значение (сколько раз элемент повторяется)
 
 const array: string[] = ["orange", "apple", "banana", "apple", "orange", "orange"];
 
-const result: Record<string, number> = { orange: 3, apple: 2, banana: 1 };
+//const result: Record<string, number> = { orange: 3, apple: 2, banana: 1 };
+
+function parseArrray(arr: string[]) {
+  const result: Record<string, number> = {};
+  for (const value of arr) {
+    if (result[value]) {
+      result[value]++;
+    } else {
+      result[value] = 1;
+    }
+  }
+  return result;
+}
+console.log(parseArrray(array));
